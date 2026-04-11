db9db0582319adc30f16f67e4d2c38a29c3ee5e9
buzzaggwd <125117936+buzzaggwd@users.noreply.github.com>
2026-04-12 08:10:04 +1000

    Мелкие визуальные исправления

--- scanner/frontend/views.py
+++ scanner/frontend/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 from backend.models import Vocabulary
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {'active_page': 'home'})
 
 def dictionary(request):
     words = Vocabulary.objects.all()
-    return render(request, "dictionary.html", {'words': words})
+    return render(request, "dictionary.html", {'words': words, 'active_page': 'dictionary'})
 
 def word(request):
-    return render(request, "word.html")
+    return render(request, "word.html", {'active_page': 'word'})
 
 def levels(request):
-    return render(request, "levels.html")
+    return render(request, "levels.html", {'active_page': 'levels'})
 
 def level(request):
-    return render(request, "level.html")
+    return render(request, "level.html", {'active_page': 'levels'})
 
 def profile(request):
-    return render(request, "profile.html")
+    return render(request, "profile.html", {'active_page': 'profile'})
 
 def ar_scanner(request):
-    return render(request, "ar_scanner.html")
+    return render(request, "ar_scanner.html", {'active_page': 'home'})
