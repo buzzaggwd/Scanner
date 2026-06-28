eecc11ae582cfcaf49f18353beddfd384216b89e
buzzaggwd <125117936+buzzaggwd@users.noreply.github.com>
2026-06-28 13:15:09 +1000

    Фикс миграции

--- scanner/backend/admin.py
+++ scanner/backend/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import User, Vocabulary, User_to_vocab, Ar_markers, Lessons, Lesson_content, User_to_lessons
+from .models import User, Vocabulary, User_to_vocab, Lessons, Lesson_content, User_to_lessons
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'telegram_id', 'username', 'avatar_url', 'experience_points', 'created_at', 'updated_at')
+    list_display = ('id', 'username', 'avatar_url', 'experience_points', 'created_at', 'updated_at')
 
 @admin.register(Vocabulary)
 class VocabularyAdmin(admin.ModelAdmin):
@@ -12,10 +12,6 @@ class VocabularyAdmin(admin.ModelAdmin):
 @admin.register(User_to_vocab)
 class User_to_vocabAdmin(admin.ModelAdmin):
     list_display = ('id', 'user_id', 'word_id', 'created_at', 'updated_at')
-
-@admin.register(Ar_markers)
-class Ar_markersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'image_hash', 'coordinates', 'description', 'type', 'created_at', 'updated_at')
 
 @admin.register(Lessons)
 class LessonsAdmin(admin.ModelAdmin):
